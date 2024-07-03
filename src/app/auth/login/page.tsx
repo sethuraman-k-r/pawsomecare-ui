e1eb26b2fe750af0a1ec3566266e67f6ca6ecaa8
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2024-07-03 13:28:15 +0530

    added sign in functionality

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,13 +3,20 @@
 import { SERVER_URL } from "@/config/config.prop";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [alertState, setAlertState] = useState(0);
+  const [alertMsg, setAlertMsg] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
 
   const submitData = async (e: any) => {
     e.preventDefault();
+    setShowAlert(false);
     let response = await fetch(`${SERVER_URL}auth/public/login`, {
       method: "POST",
       body: JSON.stringify({
@@ -21,7 +28,17 @@ export default function Login() {
       },
     });
 
-    response = await response.json();
+    setShowAlert(true);
+    if (response.status === 200) {
+      setAlertState(200);
+      setAlertMsg("Logged in successfully");
+      setTimeout(() => {
+        router.push("/");
+      }, 1000);
+    } else {
+      setAlertState(response.status);
+      setAlertMsg("Something went wrong");
+    }
   };
 
   return (
@@ -35,6 +52,34 @@ export default function Login() {
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
+        {showAlert && (
+          <div
+            className={`border px-4 py-3 mt-3 rounded relative ${
+              alertState !== 200
+                ? "bg-red-100 border-red-400 text-red-700"
+                : "bg-green-100 border-green-400 text-green-700"
+            }`}
+            role="alert"
+          >
+            <span className="block sm:inline">{alertMsg}</span>
+            <span
+              className="absolute top-0 bottom-0 right-0 px-4 py-3"
+              onClick={() => setShowAlert(false)}
+            >
+              <svg
+                className={`fill-current h-6 w-6 ${
+                  alertState !== 200 ? "text-red-500" : "text-green-500"
+                }`}
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
+        )}
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
